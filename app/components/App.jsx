0adf2e4363e9c99915bf778dbe8a35e08dd3b6a0
unknown <erichardson@CSGRALLT54.cardinalsolutions.com>
2016-02-04 16:15:59 -0500

    turned notes into a kanban board

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,46 +1,31 @@
+import AltContainer from 'alt-container';
 import React from 'react';
 
-import Notes from './Notes.jsx';
-import NoteActions from '../actions/NoteActions';
-import NoteStore from '../stores/NoteStore';
+import Lanes from './Lanes.jsx';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
 
 
 export default class App extends React.Component {
 
-    constructor(props) {
-    super(props);
-    this.state = NoteStore.getState();
-  }
-  componentDidMount() {
-      NoteStore.listen(this.storeChanged);
-  }
-  componentWillUnmount() {
-      NoteStore.unlisten(this.storeChanged);
-  }
-  storeChanged = (state) => {
-      this.setState(state);
-  };
     render() {
-        const notes = this.state.notes
         return (
             <div>
-                <button className="add-note" onClick={this.addNote}>+ Note</button>
-                <Notes notes={notes}
-                    onEdit={this.editNote}
-                    onDelete={this.deleteNote} />
+                <button className="add-lane" onClick={this.addLane}>+ Lane</button>
+                <AltContainer
+                    stores={[LaneStore]}
+                    inject={{
+                        lanes: () => LaneStore.getState().lanes || []
+                    }}
+                >
+                    <Lanes />
+                </AltContainer>
             </div>
         );
     }
 
-    addNote = () => {
-        NoteActions.create({task: 'New Task'});
+    addLane = () => {
+        LaneActions.create({name: 'New Lane'});
     };
 
-    editNote = (id, task) => {
-        NoteActions.update({id, task});
-    };
-
-    deleteNote = (id) => {
-        NoteActions.delete(id);
-    };
 }
